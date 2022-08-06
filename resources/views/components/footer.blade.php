f521f6beb9497b249b72cb05d106a6574926e460
Kleizjd <jose.jdgo98@gmail.com>
2022-08-06 14:17:21 -0500

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            <span> &copy;Copyright <?= date("Y"); ?> Best -José Daniel Grijalba</span>
+          </div>
+            <div class="clearfix"></div>
+          </footer>
+          <!-- /footer content -->
